e750608d6543f36e908fc161c803d4b501127768
Gautier François <gautierfrancois+apkiq@ik.me>
2023-02-18 22:21:25 +0100

    feat: minima DAG algorithm

--- models/pipeline.rb
+++ models/pipeline.rb
@@ -8,6 +8,26 @@ class Pipeline
 
     @executor = Executor.new(schema[:executor], @base)
   end
+
+  def ordered_tasks
+    all_tasks = []
+    @tasks.each do |task|
+      if task.depends_on
+        task.depends_on.each do |subtask|
+          @tasks.each do |st|
+            if st.name == subtask
+              all_tasks.append(st)
+            end
+          end
+        end
+      else
+        all_tasks.append(task)
+      end
+    end
+
+    all_tasks
+  end
+
   def start!
     tasks = ordered_tasks
     begin
