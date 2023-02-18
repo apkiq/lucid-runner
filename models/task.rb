a653b32a6a359346d0e927056077d498ee41e0de
Gautier François <gautierfrancois+apkiq@ik.me>
2023-02-18 22:20:55 +0100

    refactor: define dependencies from tasks

--- models/task.rb
+++ models/task.rb
@@ -1,10 +1,14 @@
 class Task
+  attr_reader :depends_on
+  attr_reader :commands
+
   def initialize(task_schema)
     @schema = task_schema
     @name = task_schema[:name]
     @ignore_fail = task_schema[:ignore_fail]
+    @depends_on = task_schema[:depends_on]
     @commands = task_schema[:commands].map do |command|
       Command.new(command)
-    end
+    end  
   end
 end
